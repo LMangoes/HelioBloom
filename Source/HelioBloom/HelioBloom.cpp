369be6059dad6b96b639e6c159cf212335b3c5f8
LMangoes <136444819+LMangoes@users.noreply.github.com>
2025-10-17 12:00:01 +0200

    [wip][dev] build on Work PC

--- Source/HelioBloom/HelioBloom.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "HelioBloom.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HelioBloom, "HelioBloom" );
-
-DEFINE_LOG_CATEGORY(LogHelioBloom)
--- Source/HelioBloom/HelioBloomGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "HelioBloomGameMode.h"
-
-AHelioBloomGameMode::AHelioBloomGameMode()
-{
-	// stub
-}
